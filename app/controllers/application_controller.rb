cf7056efecd1a42aa1e14523072bbc6584100a3d
Nadia Bajwa <nadiabajwa@Nadias-MacBook-Pro.local>
2015-12-06 20:58:06 -0600

    Adding active model serializers to the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+
+  include ActionController::Serialization
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 end
